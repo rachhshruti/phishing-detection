b90147d9a40270379b87455160b0f85644bd818d
rachhshruti <rachhshruts@gmail.com>
2017-12-06 16:17:43 -0600

    Fixed bugs when extracting links from Gmail

--- linkguard/send_links.js
+++ linkguard/send_links.js
@@ -1,21 +1,22 @@
 /* 
 Gets the actual and visual links from the email body and sends them back to 
 popup.js where they are further processed
-@author Shruti Rachh
+@author Shruti Rachh, Mrunal Mahajan
 */
-var a = (document.getElementsByClassName('ii gt adP adO')[0].innerHTML);
+var emailBody = (document.getElementsByClassName("ii gt adP adO")[0].innerHTML);
 
-var locationStart = a.indexOf('<a');
-var locationend = a.indexOf('</a>');
-
-// Extracts the actual link i.e. the link that user gets actually redirected to (not visible)
-var finalString =a.substring(locationStart,locationend);
-var n=finalString.split(/[">]/);
-var alink=n[1];
+var locationStart = emailBody.indexOf('<a');
+var locationEnd = emailBody.indexOf('</a>');
+var hrefLink = emailBody.substring(locationStart,locationEnd);
 
 // Extracts the visual link, the text or link visible to user
-var f_pos=a.indexOf("blank",0);
-var vlink=a.substring((f_pos+7),locationend);
+var n = hrefLink.split(/[">]/);
+var vlink = n[1];
+
+// Extracts the actual link i.e. the link that user gets actually redirected to (not visible)
+locationStart = hrefLink.indexOf('href=')
+locationEnd = hrefLink.indexOf(' target')
+var alink = hrefLink.substring(locationStart+6,locationEnd-1);
 
 // Sends the request with the links which is then handled by listener on popup.js
 var links = alink+" "+vlink;
